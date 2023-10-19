afc1b26ad8a19a3c57bcf8340775e047f3bf76cf
Wajiha Arain <88322471+WajihaHA61198@users.noreply.github.com>
2023-10-19 14:48:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+// controllers-routes-index.js
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+// data imports
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = 5001 || 9000;
+mongoose
+  .connect("mongodb+srv://wajihahanif18:terebinYW123@cluster0.xt3jcqs.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    // useNewUrlParser : The underlying MongoDB driver has deprecated their current connection string parser. Because this is a major change, they added 
+    // the useNewUrlParser flag to allow users to fall back to the old parser if they find a bug in the new parser.
+    useUnifiedTopology: true,
+    // useUnifiedTopology: False by default. Set to true to opt in to using the MongoDB driver's new connection management engine. You should set this 
+    // option to true , except for the unlikely case that it prevents you from maintaining a stable connection.
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ONLY ADD DATA ONE TIME */
+    // AffiliateStat.insertMany(dataAffiliateStat);
+    // OverallStat.insertMany(dataOverallStat);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
+    // User.insertMany(dataUser);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
